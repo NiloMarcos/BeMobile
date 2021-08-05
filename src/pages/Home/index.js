bb910dfbfee8570b85d5bf5abe5acfb916b2d5d6
Nilo Marcos <nilo.n2m15@gmail.com>
2021-08-05 12:05:37 -0300

    pegando informacoes da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from 'react';
 import Header from '../../components/Header';
 import { AiOutlineSearch } from 'react-icons/ai';
 import api from '../../services/api';
-import {Container, ContainerSearch, Title, Form, Input, SubmitButton, ContainerApi, ContainerApiInfo, ArticleApi, EmployeeDetails, ContainerDescriptions, TitleDescriptions, ImageApi, TextApi} from './styles';
+import {Container, ContainerSearch, Title, Form, Input, SubmitButton, ContainerApi, ContainerApiInfo, ArticleApi, EmployeeDetails, ContainerDescriptions, TitleDescriptions, ImageApi, TextName, TextJob, TextAdmission, TextPhone} from './styles';
 
 export default function Home(){
   const [employess, setEmployess] = useState([]);
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     async function loadEmployess(){
@@ -17,7 +18,6 @@ export default function Home(){
     loadEmployess();
   },[]);
 
-
   return(
     <Container>
       <Header />
@@ -25,31 +25,32 @@ export default function Home(){
       <ContainerSearch>
         <Title>Funcionários</Title>
         
-        <Form onSubmit={() => {}}>
-          <Input placeholder="Pesquisar" maxLength="26"/>
-          <SubmitButton>
+        <Form>
+          <Input type="text" placeholder="Pesquisar" maxLength="25" value={input} onChange={(event) => setInput(event.target.value)}/>
+          <SubmitButton onClick={() => {}}>
             <AiOutlineSearch color="#000" size={24}/>
           </SubmitButton>
         </Form>
       
         <ContainerApi>
-        <ContainerDescriptions>
-          <TitleDescriptions>Foto</TitleDescriptions>
-          <TitleDescriptions>Nome</TitleDescriptions>
-          <TitleDescriptions>Cargo</TitleDescriptions>
-          <TitleDescriptions>Data de Admissao</TitleDescriptions>
-          <TitleDescriptions>Telefone</TitleDescriptions>
-        </ContainerDescriptions>
+          <ContainerDescriptions>
+            <TitleDescriptions>Foto</TitleDescriptions>
+            <TitleDescriptions>Nome</TitleDescriptions>
+            <TitleDescriptions>Cargo</TitleDescriptions>
+            <TitleDescriptions>Data de Admissao</TitleDescriptions>
+            <TitleDescriptions>Telefone</TitleDescriptions>
+          </ContainerDescriptions>
+
           <ContainerApiInfo>
             {employess.map((item) => {
               return (
                 <ArticleApi key={item.id}>
                   <EmployeeDetails>
                     <ImageApi>{item.photo}</ImageApi>
-                    <TextApi>{item.name}</TextApi>
-                    <TextApi>{item.job}</TextApi>
-                    <TextApi>{item.admission_date}</TextApi>
-                    <TextApi>{item.phone}</TextApi>
+                    <TextName>{item.name}</TextName>
+                    <TextJob>{item.job}</TextJob>
+                    <TextAdmission>{item.admission_date}</TextAdmission>
+                    <TextPhone>{item.phone}</TextPhone>
                   </EmployeeDetails>
                 </ArticleApi>
               )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -50,31 +50,13 @@ export const SubmitButton = styled.button`
 
 export const ContainerApi = styled.div`
   position: absolute;
-  margin-top: 30rem;
-  margin-left: 1%;
-  width: 98%; 
+  width: 96.5%; 
+  margin-top: 45rem;
+  margin-left: 32px;
+
   border: 1px solid lightgray;
-`;
-
-export const ContainerApiInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  
-`;
-
-export const ArticleApi = styled.article`
-  align-items: center;
-  background-color: #FFFFFF;
-  width: 1000px;
-  height: 35px;
-  
-`;
-
-export const EmployeeDetails = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  border: 1px solid lightgray;  
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
 `;
 
 export const ContainerDescriptions = styled.div`
@@ -86,24 +68,52 @@ export const ContainerDescriptions = styled.div`
   width: 100%;
   height: 49px;
   background-color: #5984C0;
-  
-  font-size: 16px;
-  color: #FFFFFF;
 
   border-top-left-radius: 4px;
   border-top-right-radius: 4px;
 `; 
 
 export const TitleDescriptions = styled.p`
+  font-size: 16px;
+  color: #FFFFFF;
+`;
 
+export const ContainerApiInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ArticleApi = styled.article`
+  background-color: #FFFFFF;
+  width: 100%;
+  height: 55px;
+`;
+
+export const EmployeeDetails = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 55px;
+  border: 1px solid lightgray;  
 `;
 
 export const ImageApi = styled.img`
   width: 39px;
   height: 33px;
-  
 `;
 
-export const TextApi = styled.p`
+export const TextName = styled.p`
+  text-align: left;
+`;
 
+export const TextJob = styled.p`
+  text-align: left;
+`;
+
+export const  TextAdmission = styled.p`
+  text-align: left;
+`;
+
+export const  TextPhone = styled.p`
+  text-align: left;
 `;
